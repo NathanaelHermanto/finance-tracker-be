e3398e71160f19fc94defd0566f6ff40fac034f5
Nathanael Hermanto <nathanael887@gmail.com>
2024-01-28 13:01:28 +0700

    o

--- functions/App.js
+++ functions/App.js
@@ -1,16 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- *
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
- */
-
 const functions = require("firebase-functions");
 const cors = require("cors");
 const express = require("express");
