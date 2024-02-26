45e812b44fe07780bfb6e629f7de46f03a4104f0
Nathanael Hermanto <nathanael887@gmail.com>
2024-02-26 15:01:26 +0700

    sell

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,88 +1,65 @@
 const asyncHandler = require("express-async-handler");
 const supabase = require("../services/ClientService");
+const { validationResult } = require("express-validator");
 
-// transaction list
-exports.get_transaction_list = asyncHandler(
-  async (req, res, next) => {
-    try {
-      const { data, error } = await supabase
-        .from('transactions')
-        .select('*');
+exports.get_transaction_list = asyncHandler(async (req, res, next) => {
+  try {
+    const { data, error } = await supabase.from("transactions").select("*");
 
-      if (error) {
-        throw new Error(`Supabase query failed: ${error.message}`);
-      }
-
-      res.status(200).json({ data });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    if (error) {
+      throw new Error(`Supabase query failed: ${error.message}`);
     }
+
+    res.status(200).json({ data });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
+exports.get_transaction_by_id = asyncHandler(async (req, res, next) => {
+  res.send(`TODO return transaction by id: ${req.params.id}`);
+});
 
-// transaction
-exports.get_transaction_by_id = asyncHandler(
-    async (req, res, next) => {
-      res.send(`TODO return transaction by id: ${req.params.id}`);
-    });
+exports.post_inject_money = asyncHandler(async (req, res, next) => {
+  res.send("TODO inject money");
+});
 
-exports.post_inject_money = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO inject money");
-    });
+exports.post_sell = asyncHandler(async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send(`TODO sell ${req.body.amount} ${req.body.inventory_amount}`);
+});
 
-exports.post_sell_cash = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO sell on cash");
-    });
+exports.post_sell_delivery = asyncHandler(async (req, res, next) => {
+  res.send("TODO sell delivery");
+});
 
-exports.post_sell_credit = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO sell on credit");
-    });
+exports.post_buy = asyncHandler(async (req, res, next) => {
+  res.send("TODO buy");
+});
 
-exports.post_buy = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO buy");
-    });
+exports.patch_transaction_by_id = asyncHandler(async (req, res, next) => {
+  res.send("TODO update/patch transaction by id");
+});
 
-exports.patch_transaction_by_id = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO update/patch transaction by id");
-    });
+exports.delete_transaction_by_id = asyncHandler(async (req, res, next) => {
+  res.send("TODO delete transaction by id");
+});
 
-exports.delete_transaction_by_id = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO delete transaction by id");
-    });
+exports.get_balance = asyncHandler(async (req, res, next) => {
+  try {
+    const { data, error } = await supabase.rpc("get_balance");
 
+    if (error) {
+      throw new Error(`Supabase query failed: ${error.message}`);
+    }
 
-// / balance
-exports.get_balance = asyncHandler(
-    async (req, res, next) => {
-      try {
-        const { data, error } = await supabase
-          .rpc('get_balance');
-  
-        if (error) {
-          throw new Error(`Supabase query failed: ${error.message}`);
-        }
-  
-        res.status(200).json({ data });
-      } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-    });
-
-exports.get_cash_balance = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO return cash balance");
-    });
-
-exports.get_credit_balance = asyncHandler(
-    async (req, res, next) => {
-      res.send("TODO return credit balance");
-    });
+    res.status(200).json({ data });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
--- routes/TransactionRoutes.js
+++ routes/TransactionRoutes.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const { body } = require('express-validator');
 const router = express.Router();
 
 const transactionController = require("../controllers/TransactionController");
 
 router.get("/", transactionController.get_transaction_list);
 router.get("/balance", transactionController.get_balance);
-router.get("/cash-balance", transactionController.get_cash_balance);
-router.get("/credit-balance", transactionController.get_credit_balance);
 router.get("/:id", transactionController.get_transaction_by_id);
 
 router.post("/inject-money", transactionController.post_inject_money);
-router.post("/sell-cash", transactionController.post_sell_cash);
-router.post("/sell-credit", transactionController.post_sell_credit);
+router.post(
+  "/sell",
+  [
+    body("amount").notEmpty().withMessage("Amount is required"),
+    body("inventory_amount")
+      .notEmpty()
+      .withMessage("Inventory amount is required"),
+  ],
+  transactionController.post_sell
+);
+router.post("/sell-delivery", transactionController.post_sell_delivery);
 router.post("/buy", transactionController.post_buy);
 
 router.patch("/:id", transactionController.patch_transaction_by_id);
